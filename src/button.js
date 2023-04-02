5082f3093b916e7570f8486d282b9b2923eec404
יהודית ליפשיץ <ylifshit@g.jct.ac.il>
2023-04-02 14:26:49 +0300

    02/04/23

--- src/button.js
+++ src/button.js
@@ -1,7 +1,8 @@
 import React from "react";
-import  ReactDOM  from "react-dom";
+import ReactDOM from "react-dom";
 import './App.css';
 import PropTypes from "prop-types";
+import Screen from "./screen";
 
 
 export class Button extends React.Component {
@@ -10,25 +11,27 @@ export class Button extends React.Component {
     clickHandler: PropTypes.func,
   };
 
-  
-  handleClick = () => {
-    
-    ReactDOM.render(this.props.content, document.getElementById('Screen'));
 
+  handleClick = () => {
+
+   // ReactDOM.render(this.props.content, document.getElementById('Screen'));
+    Screen.setState({text: text+this.props.content});
     //this.props.clickHandler();
     //window.print();
   };
 
 
-render(){return (
-  <div className="buttonClass">
-    
-     <button style={{border: 'solid 2px black ', width:'100px', height:'50px', margin:'auto' }} onClick={this.handleClick}>{this.props.content}</button>
-    {}
-  </div>
-);}
-    
+  render() {
+    return (
+      <div className="buttonClass">
+
+        <button style={{ border: 'solid 2px black ', width: '100px', height: '50px', margin: 'auto' }} onClick={this.handleClick}>{this.props.content}</button>
+        { }
+      </div>
+    );
   }
+
+}
 
 
 
--- src/screen.js
+++ src/screen.js
@@ -3,21 +3,21 @@ import Keyboard from './keyboard';
 import React from 'react';
 
 export class Screen extends React.Component {
-    
-    constructor(props){
-        super(props);
-        this.state = {
-           text:[]
 
-        };
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: []
+    };
+  }
+  
+  render() {
+    return (
+      <div className="ScreenClass" id="Screen" style={{ border: 'solid 2px black ', width: '500px', height: '250px', marginBottom: 0 }} >
 
-    }
-    render()
-{return (
-  <div className="ScreenClass" id="Screen" style={{border: 'solid 2px black ', width:'500px', height:'250px', marginBottom:0}} >
-   
-  </div>
-);}
+      </div>
+    );
+  }
 
 }
 export default Screen;
