f3ece3edb9ea2041480d10e0fac670cf097abbe5
LeaScetbon <73163251+LeaScetbon@users.noreply.github.com>
2023-04-04 11:38:31 +0300

    Update screen.js

--- src/screen.js
+++ src/screen.js
@@ -8,7 +8,6 @@ export class Screen extends React.Component {
   render() {
     return (
       <div className="ScreenClass" id="Screen" clickHandler={this.handleClick} style={{ border: 'solid 2px black ', width: '500px', height: '250px', marginBottom: 0 }} >
-        <p>{this.state.text}</p>
       </div>
     );
   }
