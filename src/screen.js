046dac9e00e7601969344ec004880ecf183a113d
LeaScetbon <73163251+LeaScetbon@users.noreply.github.com>
2023-04-17 12:41:24 +0300

    merge

--- src/screen.js
+++ src/screen.js
@@ -1,15 +1,27 @@
 import './App.css';
+import Keyboard from './keyboard';
 import React from 'react';
 
 export class Screen extends React.Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: []
+    };
+  }
+
+  screenHandleClick = content => {
+    this.setState({ text: this.state.text + content });
+  };
+
   render() {
     return (
-      <div style={{backgroundColor:"white"}}>
-        <p className="ScreenClass" id="Screen" clickHandler={this.handleClick} style={{ fontSize: '20px', border: 'solid 2px black ', width: '800px', height: '350px', margin: 'auto' }} >
-        </p>
+      <div className="ScreenClass" id="Screen" clickHandler={this.handleClick} style={{ border: 'solid 2px black ', width: '500px', height: '250px', marginBottom: 0 }} >
+        <p>{this.state.text}</p>
       </div>
     );
   }
+
 }
 export default Screen;
